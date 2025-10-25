5fe41f0b59fa38b9963df5aa2db85e18111ab147
Samar Singla <169121400+SamarSingla0@users.noreply.github.com>
2025-10-25 13:58:30 +0530

    Set DEBUG to True in settings.py
    Changed DEBUG setting from False to True for development.

--- ai_guardian/ai_guardian/settings.py
+++ ai_guardian/ai_guardian/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'unique1445.pythonanywhere.com']
 
